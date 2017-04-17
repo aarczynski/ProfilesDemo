ba8cfb3e05491e39f7d87da5f02cfbed2a15bc8a
Adam Arczynski <adam.arczynski@ocado.com>
2017-04-17 16:33:31 +0200

    bootstrap Sping Boot app

---
+++ src/main/java/pl/arczynskiadam/demo/appprofiles/ProfilesDemoApplication.java
@@ -0,0 +1,12 @@
+package pl.arczynskiadam.demo.appprofiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfilesDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfilesDemoApplication.class, args);
+	}
+}
