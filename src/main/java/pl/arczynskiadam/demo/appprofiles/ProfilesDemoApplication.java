4b5281b0fa050ca0d65489b3c3b9bee3675fecdb
Adam Arczynski <adam.arczynski@gmail.com>
2017-04-17 19:56:29 +0200

    add profiles to application

--- src/main/java/pl/arczynskiadam/demo/appprofiles/ProfilesDemoApplication.java
+++ src/main/java/pl/arczynskiadam/demo/appprofiles/ProfilesDemoApplication.java
@@ -2,11 +2,15 @@ package pl.arczynskiadam.demo.appprofiles;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import pl.arczynskiadam.demo.appprofiles.service.DemoService;
 
 @SpringBootApplication
 public class ProfilesDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProfilesDemoApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(ProfilesDemoApplication.class, args);
+		DemoService demoService = (DemoService) context.getBean("demoService");
+		demoService.action();
 	}
 }
